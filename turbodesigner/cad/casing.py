cfd38736d15052142fde7047104208a373bf82ad
Afshawn Lotfi <6283745+afshawnlotfi@users.noreply.github.com>
2022-12-15 22:59:05 -0800

    Finished adding stator blades to casing (#7)

--- turbodesigner/cad/casing.py
+++ turbodesigner/cad/casing.py
@@ -1,7 +1,10 @@
 from dataclasses import dataclass
+from turbodesigner.cad.blade import BladeCadModel
 from turbodesigner.cad.common import ExtendedWorkplane
 from turbodesigner.stage import StageExport
 import cadquery as cq
+
+from turbodesigner.turbomachinery import TurbomachineryExport
 
 
 @dataclass
@@ -16,6 +19,9 @@ class CasingCadModel:
         casing_thickness = self.thickness_to_inlet_radius * first_stage.rotor.tip_radius
         transition_height = stage.stage_height * self.transition_to_total_height
         casing_height = stage.stage_height + transition_height
+        stator_blade = BladeCadModel.blade_profile(stage.stator)
+        blade_height_offset = stage.stator.disk_height/2
+        
         path = (
             cq.Workplane("XZ")
             .moveTo(0, 0)
@@ -36,4 +42,28 @@ class CasingCadModel:
                 .circle(stage.rotor.tip_radius)
                 .sweep(path, multisection=True, makeSolid=True)
             )
+            .add(
+                cq.Workplane("XY")
+                .polarArray(stage.stator.hub_radius, 0, 360, stage.stator.number_of_blades)
+                .eachpoint(
+                    lambda loc: (
+                        stator_blade
+                        .translate((-blade_height_offset,0,0))
+                        .rotate((0,0,0), (0,1,0), 90)
+                    ).val().located(loc), True)  # type: ignore
+            )
+
         )
+
+    def casing_assembly(self, turbomachinery: TurbomachineryExport):
+        assembly = cq.Assembly()
+        stage_height_offset = 0
+
+        first_stage = turbomachinery.stages[0]
+        for i in range(0, len(turbomachinery.stages)):
+            current_stage = turbomachinery.stages[i]
+            stage_height_offset -= current_stage.stage_height * (1+self.transition_to_total_height)
+            stage_shaft_profile = self.casing_profile(current_stage, first_stage)
+            assembly.add(stage_shaft_profile, loc=cq.Location(cq.Vector(0, 0, stage_height_offset)), name=f"Stage {current_stage.stage_number}")
+
+        return assembly
